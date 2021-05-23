1f9bc8d2459ad5bc1aaf7cb36e5bdfea2bc02144
Malgrin <ctapuk_87@mail.ru>
2021-05-24 01:46:16 +0300

    Task complete

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,26 @@
 package subtask1
 
+/*  Массив целых чисел «печален», поскольку содержит плохие элементы.
+    Элемент массива считается плохим, если сумма ближайших элементов меньше его значения.
+* */
+
 class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val buffer = sadArray.toMutableList()
+        var sad = true
+        while (sad && buffer.size > 2) {
+            for (i in 1..buffer.size - 2) {
+                if (buffer[i] > buffer[i - 1] + buffer[i + 1]) {
+                    buffer.removeAt(i)
+                    break
+                } else if (i == buffer.size - 2 || buffer.size < 3){
+                    sad = false
+                }
+            }
+        }
+        return buffer.toIntArray()
     }
 }
+
--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,9 +1,68 @@
 package subtask3
 
+/*  Дана строка с открывающими-закрывающими скобками, то есть [], <> или (). Найдите все подстроки подстроки,
+которые встречаются между скобками.
+Обратите внимание, что подстрока должна находиться между первой встреченной открытой и закрывающей скобками.
+Например, для «(вот) какой-то текст)» ввод будет «вот», а не «(вот) какой-то текст)» из-за второй скобки «)».
+
+Вход: строка, которая может содержать следующие скобки: [], <>, ()
+
+Выход: массив строк. Каждый элемент массива должен содержать подстроку между открывающей-закрывающей скобкой.
+
+Пример ввода:
+
+«Это [пример] (строка)»
+
+Пример вывода:
+
+[«Ан», «пример», «строка»]
+
+Nota Bene: Пожалуйста, не забывайте обрабатывать ситуацию с вложенными скобками (например, «(ipsum [dolor <] amet)>»).
+* */
+
 class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val buffer = mutableListOf<String>()
+        var treatStr: String
+        var symbolEnd: Char
+        var symbolStart: Char
+        var index = 0
+        var check = 0
+        for (i in inputString.indices) {
+            if (inputString[i] == '(' || inputString[i] == '<' || inputString[i] == '[') {
+                symbolStart = inputString[i]
+                symbolEnd = if (symbolStart == '(') {
+                    inputString[i] + 1
+                } else {
+                    inputString[i] + 2
+                }
+
+                treatStr = inputString.substring(i + 1)
+                for (y in treatStr.indices) {
+                    if (treatStr[y] == symbolStart) {
+                        check += 1
+                    }
+                }
+                for (j in treatStr.indices) {
+
+                    if (treatStr[j] == symbolEnd) {
+                        check -= 1
+                        if (check == 0 || check == -1) {
+                            index = j
+                            check = 0
+                            break
+                        }
+                    }
+                }
+                buffer.add(treatStr.substring(0,index))
+            }
+
+        }
+        for (i in buffer.indices) {
+            println(buffer[i])
+        }
+        return buffer.toTypedArray()
     }
 }
